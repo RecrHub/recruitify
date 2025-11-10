e1fc3c0483387221316ec3e14a30fc6b20d64b06
Administrator <hungthanhhung37@gmail.com>
2025-11-11 00:57:01 +0700

    feat : recr-84-create-backend-job-posting-entity-and-repository feat : implements entities job , ward , province , AdministrativeUnit , WorkApproach , EmploymentType , ExperienceLevel , Company , Category , Skill and IJobRepository

---
+++ apps/backend/src/main/java/com/recruitify/repository/IJobRepository.java
@@ -0,0 +1,8 @@
+package com.recruitify.repository;
+
+import com.recruitify.model.Job;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+public interface IJobRepository extends JpaRepository<Job,Long>, JpaSpecificationExecutor<Job> {
+}
