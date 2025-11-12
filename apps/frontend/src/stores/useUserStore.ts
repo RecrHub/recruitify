75a78deefcabc1a947670cbab35a8d4f9962412e
minhphi <phiminh131205@gmail.com>
2025-11-13 00:39:26 +0700

    feat: recr-69 Create User Store

---
+++ apps/frontend/src/stores/useUserStore.ts
@@ -0,0 +1,45 @@
+import { create } from 'zustand'
+import { persist } from 'zustand/middleware'
+
+export interface User {
+  id: string
+  name: string
+  email: string
+  role?: string
+}
+
+interface UserState {
+  user: User | null
+  token: string | null
+  isAuthenticated: boolean
+  setUser: (user: User, token: string) => void
+  //logout: () => void
+}
+
+export const useUserStore = create<UserState>()(
+  persist(
+    (set) => ({
+      user: null,
+      token: null,
+      isAuthenticated: false,
+
+      setUser: (user, token) =>
+        set(() => ({
+          user,
+          token,
+          isAuthenticated: true,
+        })),
+
+      logout: () =>
+        set(() => ({
+          user: null,
+          token: null,
+          isAuthenticated: false,
+        })),
+    }),
+    {
+      name: 'user-storage', // Lưu token + user vào localStorage
+      partialize: (state) => ({ token: state.token, user: state.user }),
+    }
+  )
+)
