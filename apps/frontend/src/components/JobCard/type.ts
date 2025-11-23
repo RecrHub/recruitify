e4652fec9a747b1e370c5ca7fe46d0928980813d
Administrator <hungthanhhung37@gmail.com>
2025-11-23 13:34:28 +0700

    feat : recr-94 Update Homepage to Display Job Listings feat: Add FeatureJob component and integrate it into the homepage
    - Introduced FeatureJob component to showcase featured job listings.
    - Updated HeroSection to include the new FeatureJob component.
    - Created JobCard component to display individual job details.
    - Implemented Grid component for responsive layout of job cards.
    - Added mock data for job listings to be used in the FeatureJob component.
    - Created styles for FeatureJob, JobCard, and Grid components for consistent UI.
    - Added new SVG icons for bookmarks, briefcase, and location.

---
+++ apps/frontend/src/components/JobCard/type.ts
@@ -0,0 +1,10 @@
+export interface JobCardProps {
+    id: number;
+    title: string;
+    company: string;
+    salary: string;
+    location: string;
+    workTime: string;
+    postedDate: string;
+    icon?: string;
+}
