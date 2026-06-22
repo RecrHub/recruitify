f8ebe04f55ddbd8fa7245531d67e095582c69095
Administrator <hungthanhhung37@gmail.com>
2026-06-22 22:21:27 +0700

    chore: add comment for author attribution in robots.ts

--- frontend/src/app/robots.ts
+++ frontend/src/app/robots.ts
@@ -8,5 +8,6 @@ export default function robots(): MetadataRoute.Robots {
       disallow: ['/api/', '/login', '/signup'],
     },
     sitemap: 'https://recruitify.com/sitemap.xml',
+    // Hung Heo
   };
 }
